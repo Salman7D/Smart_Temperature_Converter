6ddbd307a23bb269045999487897d053690d0b27
Salman7D <salmanrayhan753@gmail.com>
2026-09-01 23:08:20 +0600

    Create temperature converter

--- Smart_Temperature_Converter.dart
+++ Smart_Temperature_Converter.dart
@@ -1,3 +1,15 @@
-main(){
-  print("Temperature");
+void main() {
+  while (true) {
+    print('====================================');
+    print('       SMART TEMPERATURE CONVERTER');
+    print('====================================');
+
+    print('1. Celsius to Fahrenheit');
+    print('2. Fahrenheit to Celsius');
+    print('3. Celsius to Kelvin');
+    print('4. Kelvin to Celsius');
+    print('5. Fahrenheit to Kelvin');
+    print('6. Kelvin to Fahrenheit');
+    print('7. Exit');
+  }
 }
